e7ebf296488232c0972d5bbfa58679d2254c87cb
Shravan Shrivastav <shravans@chetu.com>
2019-07-12 18:30:34 +0530

    Added Test class

---
+++ gittest/src/gittest/Test.java
@@ -0,0 +1,11 @@
+ /**
+ * Class Information
+ * @author shravans
+ * @version 1.2 - 12-Jul-2019
+ */
+ package gittest;
+ public class Test {
+	 public static void main(String[] args) {
+		System.out.println("Git test");
+	}
+}
