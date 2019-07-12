3d891312cea87727885192c1deac762c486b3960
Shravan Shrivastav <shravans@chetu.com>
2019-07-12 19:33:56 +0530

    Added branch1 class

---
+++ gittest/src/gittest/Branch1.java
@@ -0,0 +1,12 @@
+/**
+* Class Information
+* @author shravans
+* @version 1.2 - 12-Jul-2019
+*/
+package gittest;
+
+public class Branch1 {
+	public static void test() {
+		System.out.println("Branch1");
+	}
+}
